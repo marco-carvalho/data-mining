da3e685e03a46d1835350889962126831cf4c467
Olfmor <olfmor@yahoo.com.br>
2017-06-07 14:32:04 -0300

    initial commit

---
+++ zika.R
@@ -0,0 +1,149 @@
+setwd("C:\\Users\\Marco\\Google Drive\\Prática\\Identificação de Padrões com Dados do Zika")
+zika_old = read.csv("linkage.csv", header = TRUE, sep = ";", fileEncoding = 'utf-8')
+
+################################################################################
+# criando data.frame com os dados do csv selecionados para utilização
+################################################################################
+
+zika_new <- data.frame(
+  zika_old$NU_IDADE_GESTANTE,
+  zika_old$CO_RACA_COR,
+  zika_old$SG_UF_RESIDENCIA,
+  zika_old$CO_MUN_RESIDENCIA,
+  zika_old$SEXO,
+  zika_old$DTNASC,
+  zika_old$DTNASC,
+  zika_old$PESO,
+  zika_old$NU_COMPRIMENTO,
+  zika_old$NU_PERIMETRO_CEFALICO,
+  zika_old$TP_CLASSIFICACAO_FETO_RN,
+  zika_old$TP_GRAVIDEZ,
+  zika_old$TP_CLASSIFICACAO_FINAL,
+  zika_old$ST_PRESENCA_FEBRE,
+  zika_old$ST_PRESENCA_EXANTEMA,
+  zika_old$ST_REALIZACAO_EXAME_TORSCH,
+  zika_old$ST_REALIZACAO_EXAME_DCZ,
+  zika_old$ST_OBITO
+)
+
+################################################################################
+# renomeando as colunas do novo data.frame
+################################################################################
+
+colnames(zika_new) = c(
+  "idade_mae",
+  "raca_mae",
+  "UF_mae",
+  "mun_mae",
+  "sexo_filho",
+  "ano_nasc_filho",
+  "mes_nasc_filho",
+  "peso_filho",
+  "comprimento_filho",
+  "perimetro_cefalico_filho",
+  "classificacao_feto_gestacao",
+  "tipo_gravidez_gestacao",
+  "classificacao_gestacao",
+  "febre_dados_clinicos",
+  "exantema_dados_clinicos",
+  "torsch_dados_clinicos",
+  "dcz_dados_clinicos",
+  "obito_dados_clinicos"
+)
+
+################################################################################
+# removendo valores inválidos
+################################################################################
+
+zika_new[zika_new == "FORMSUS_PE_NA"] <- NA
+zika_new[zika_new == 0] <- NA
+zika_new[zika_new == ""] <- NA
+
+################################################################################
+# a coluna de municipio de residência da gestante está com o tipo "int"
+# para corrigir isso, deve-se alterar o tipo dessa coluna para "factor"
+################################################################################
+
+zika_new$mun_mae <- as.factor(zika_new$mun_mae)
+
+################################################################################
+# as colunas comprimento e perimetro cefalico do filho estão com o tipo "factor"
+# para corrigir isso, deve-se substituir "," por "."
+# após isso, deve-se alterar o tipo dessa coluna para "numeric"
+################################################################################
+
+zika_new$comprimento_filho <- as.numeric(sub(",", ".", zika_new$comprimento_filho))
+zika_new$perimetro_cefalico_filho <- as.numeric(sub(",", ".", zika_new$perimetro_cefalico_filho))
+
+################################################################################
+# alguns registros das colunas que representam datas apresentavam inconsistência
+# a inconsistência era notada por alguns registros possuirem apenas 7 dígitos
+# o motivo disso era por conta do primeiro caractere ser "0"
+# para corrigir isso, deve-se adicionar um "0" para os registros com 7 dígitos
+# após isso, deve-se alterar o tipo dessa coluna para "date"
+################################################################################
+
+zika_new$ano_nasc_filho[which(nchar(zika_new$ano_nasc_filho) == 7)] <- paste(
+  "0", 
+  zika_new$ano_nasc_filho[which(nchar(zika_new$ano_nasc_filho) == 7)], 
+  sep = ""
+)
+zika_new$ano_nasc_filho <- as.Date(zika_new$ano_nasc_filho, "%d%m%Y")
+
+zika_new$mes_nasc_filho[which(nchar(zika_new$mes_nasc_filho) == 7)] <- paste(
+  "0", 
+  zika_new$mes_nasc_filho[which(nchar(zika_new$mes_nasc_filho) == 7)], 
+  sep = ""
+)
+zika_new$mes_nasc_filho <- as.Date(zika_new$mes_nasc_filho, "%d%m%Y")
+
+################################################################################
+# pegando a parte do mês/ano para as respectivas colunas
+################################################################################
+
+zika_new$ano_nasc_filho <- as.integer(substr(zika_new$ano_nasc_filho, 1, 4))
+zika_new$mes_nasc_filho <- as.integer(substr(zika_new$mes_nasc_filho, 6, 7))
+
+################################################################################
+# para cada coluna do data.frame
+#   se a coluna for um valor numérico
+#     enquanto existir outliers na coluna
+#       seta NA ao valor considerado outlier
+################################################################################
+
+for(i in 1:ncol(zika_new)) {
+  if(!is.factor(zika_new[,i])) {
+    while(length(boxplot(zika_new[,i])$out) != 0) {
+      zika_new[,i][zika_new[,i] %in% boxplot(zika_new[,i])$out] <- NA
+    }
+  }
+}
+
+################################################################################
+# concatenando valores do campo "obito_dados_clinicos"
+################################################################################
+
+zika_new$obito_dados_clinicos[zika_new$obito_dados_clinicos == "Obito"] <- "Sim"
+zika_new$obito_dados_clinicos[zika_new$obito_dados_clinicos == "Vivo"] <- "Nao"
+zika_new$obito_dados_clinicos[zika_new$obito_dados_clinicos == "Ignorado"] <- NA
+zika_new$obito_dados_clinicos <- droplevels(zika_new$obito_dados_clinicos)
+
+################################################################################
+# mostrar somente o texto da coluna "classificacao_gestacao"
+################################################################################
+
+zika_new$classificacao_gestacao <- substr(
+  zika_new$classificacao_gestacao, 
+  4, 
+  nchar(as.character(zika_new$classificacao_gestacao))
+)
+
+zika_new$classificacao_gestacao <- as.factor(zika_new$classificacao_gestacao)
+
+################################################################################
+# removendo conteúdo temporário
+################################################################################
+
+rm(zika_old, i)
+str(zika_new)
+boxplot(zika_new)
